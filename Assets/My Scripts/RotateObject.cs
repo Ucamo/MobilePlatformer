15b491c4fac5410b66ef2df9c8c9724a27e71212
Uriel Carrillo <carrillouriel@gmail.com>
2017-03-05 11:13:17 -0700

    Draw coin, implemented animation

---
+++ Assets/My Scripts/RotateObject.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateObject : MonoBehaviour {
+
+	public float speed;
+
+	void Start () {
+	
+	}
+
+	void Update () {
+		transform.RotateAround(transform.position, transform.up, Time.deltaTime * speed);
+	}
+}
