d6d7991a344ce3426b16a3950bf61f0b40d6ec1b
Uriel Carrillo <carrillouriel@gmail.com>
2017-03-21 23:09:12 -0700

    added splash screen

---
+++ Assets/My Scripts/Splash.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Splash : MonoBehaviour {
+	public int level = 1;    //Level to open after splash
+	public float setTime = 1.0f;    //Duration before loading next level
+	public float dimTime = 2.0f;  //Duration Before Staring to Fade or Dim Lights
+	public Light dimLight;   //Main Light Source to Dim
+	public float zoomSpeed = 0.2f;   //Speed at which camera zooms in
+	Camera c;
+	float timer;     // Use this for initialization
+	void Start () {
+		c = GetComponent<Camera> ();    //Find attached Camera Component
+		timer = 0.0f; //Initializes Timer to 0
+	}
+
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime; //Adds Time.deltaTime to timer each update
+		c.fieldOfView -= zoomSpeed; //Zooms in Camera
+		if (timer > dimTime && timer < setTime) {
+			dimLight.intensity -= zoomSpeed; //Dims Lights
+		} else if (timer > setTime) {
+			Application.LoadLevel (level); //Loads Level at index
+		}
+	}
+}
+
