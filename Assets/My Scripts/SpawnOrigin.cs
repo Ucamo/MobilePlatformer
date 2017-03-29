fd72173f58dedddc65471808625c04b3e11f0db1
Uriel Carrillo <carrillouriel@gmail.com>
2017-03-28 22:35:10 -0700

    added icon to unity project

---
+++ Assets/My Scripts/SpawnOrigin.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnOrigin : MonoBehaviour {
+
+	string spawnOrigin;
+	void Start () {
+	
+	}
+
+	public void SetSpawnOrigin(string val)
+	{
+		spawnOrigin = val;
+	}
+
+	public string GetSpawnOrigin()
+	{
+		return spawnOrigin;
+	}
+		
+}
